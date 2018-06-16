562c9fcfc8e832a7128f9451844507286b3e9707
Maximilian Fricke <mfricke2808@gmail.com>
2018-06-17 01:49:58 +0200

    Fix encoding

--- pythonx/completers/common/tags.py
+++ pythonx/completers/common/tags.py
@@ -1,10 +1,15 @@
 from completor import Completor, vim
+from completor.compat import to_unicode
+from completers.common.buffer import get_encoding
 
 class Tags(Completor):
     filetype = 'tags'
     sync = True
 
     def parse(self, base):
-        tags = vim.eval('taglist("{}")'.format(base))
+        nr = vim.current.buffer.number
+        encoding = get_encoding(nr)
+
+        tags = vim.eval('taglist("{}")'.format(to_unicode(base, encoding)))
         names = list(set([tag['name'] for tag in tags]))
         return [{'word': name, 'menu': '[Tag]'} for name in sorted(names)]
